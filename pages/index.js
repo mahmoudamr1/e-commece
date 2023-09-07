5f9d3b9e24ec490b6ac924a0e5176b24fb7ef15d
mahmoudamr1 <mahmoudamr700@gmail.com>
2023-09-07 15:11:08 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+
+
+
+import { client } from '../Lib/client';
+
+
+import Section_1 from '../components/Section_1';
+import Section_2 from '../components/Section_2';
+import Slider_2 from '../components/Slider-2';
+
+import Slider_1 from '../components/slider';
+
+
+
+const Home = ( {products,products_2,}) => (
+  <>
+    
+    <Slider_2 />
+    <Slider_1 />
+    <Section_1 product={ products} />
+    <Section_2 product={ products_2 } />
+ 
+
+
+   
+
+  </>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "sectionOne"]';
+  const products = await client.fetch( query );
+
+
+  const products_2 = await client.fetch( '*[_type == "sectionTwo"]' );
+
+  
+
+  return {
+    props: { products, products_2 }
+  }
+}
+
+export default Home;
